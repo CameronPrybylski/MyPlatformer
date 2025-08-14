a97e0316b976a1bc00a51aeb0e42dce47a280125
Cameron Przybylski <cameronprzybylski@yahoo.com>
2025-08-13 19:11:00 -0500

    Updating dyn collisions and adding level 2

--- game/src/Obstacle.cpp
+++ game/src/Obstacle.cpp
@@ -1,6 +1,6 @@
 #include <Game/Obstacle.h>
 
-Obstacle::Obstacle(glm::vec3 position, glm::vec3 scale, glm::vec3 velocity, glm::vec4 color, std::string texturePath, bool isStatic)
+Obstacle::Obstacle(glm::vec3 position, glm::vec3 scale, glm::vec3 velocity, glm::vec4 color, std::string texturePath, std::string name, bool isStatic)
 {
     //shaderName = "textureShader";
     mesh = AssetManager::GetMesh("quadMesh");
@@ -9,6 +9,7 @@ Obstacle::Obstacle(glm::vec3 position, glm::vec3 scale, glm::vec3 velocity, glm:
     rigidBody.velocity = velocity;
     rigidBody.isStatic = isStatic;
     this->color = color;
+    this->name = name;
     if(texturePath != ""){
         shaderName = "textureShader";
         texture.Create(texturePath);
@@ -30,14 +31,22 @@ void Obstacle::Update(const Input& input, float dt)
 {
     //transform.position.x += rigidBody.velocity.x * dt;
     //std::cout << rigidBody.velocity.x << std::endl;
-    if(transform.position.x - transform.scale.x / 2 < 0.0f)
+    transform.position.y = 150.0f;
+    //if(transform.position.x - transform.scale.x / 2 < 0.0f)
+    if(transform.position.x - (transform.scale.x / 2) < 600.0f)
     {
         rigidBody.velocity.x *= -1;
     }
-    if(transform.position.x > 800.0f)
+    if(transform.position.x > 1600.0f)
     {
         rigidBody.velocity.x *= -1;
     }
+    transform.position.x += rigidBody.velocity.x * dt;
+}
+
+void Obstacle::OnCollision(std::shared_ptr<GameObject> collidedObj, glm::vec2 collisionNormal, float dt)
+{
+    collidedObj->rigidBody.velocity.x = rigidBody.velocity.x;
 }
 
 void Obstacle::Render(Renderer &renderer, const Camera& camera)
--- game/src/Player.cpp
+++ game/src/Player.cpp
@@ -1,14 +1,16 @@
 #include <Game/Player.h>
 
-Player::Player(glm::vec3 position, glm::vec3 scale, glm::vec4 color, std::string texturePath, bool isStatic)
+Player::Player(glm::vec3 position, glm::vec3 scale, glm::vec4 color, std::string texturePath, std::string name, bool isStatic)
 {
     mesh = AssetManager::GetMesh("quadMesh");
     shaderName = "textureShader";
     transform.position = position;
     transform.scale = scale;
     rigidBody.isStatic = isStatic;
-    hp = 3;
+    hp = 2;
     texture.Create("/Users/cameronprzybylski/Documents/C++/C++ Projects/MyPlatformer/textures/Mario-8bit.png");
+    this->color = color;
+    this->name = name;
 }
 
 Player::~Player()
@@ -26,11 +28,12 @@ void Player::OnEvent(const Input& input)
     {
         rigidBody.velocity.x = -300.0f;
     }
-    else if(!jumping){
+    else if(!jumping && !riding){
         rigidBody.velocity.x = 0;
     }
     if(input.IsKeyDown("SPACE") && !jumping && !hit)
     {
+        riding = false;
         jumping = true;
         Jump();
     }
@@ -52,10 +55,22 @@ void Player::Update(const Input& input, float dt)
             rigidBody.velocity.x = 0;
         }
         hit = false;
-    }if(transform.position.y >= 800)
+    }else{
+        if(input.IsKeyDown("D") && !jumping && !hit)
+        {
+            riding = false;
+            rigidBody.velocity.x = 300.0f;
+        }
+        else if(input.IsKeyDown("A") && !jumping && !hit)
+        {
+            riding = false;
+            rigidBody.velocity.x = -300.0f;
+        }
+    }
+    if(transform.position.y >= 800)
     {
         transform.position.y = 800.0f;
-    }if(transform.position.y < 0.0f){
+    }if(transform.position.y < 0.0f || hp <= 0){
         alive = false;
     }
     if(transform.position.x - transform.scale.x / 2 < 0.0f){
@@ -63,14 +78,24 @@ void Player::Update(const Input& input, float dt)
     }
 }
 
-void Player::OnCollision(float dt)
+void Player::OnCollision(std::shared_ptr<GameObject> collidedObj, glm::vec2 collisionNormal, float dt)
 {
-    Hit(dt);
+    if(collisionNormal.x == 0 && collisionNormal.y == 1)
+    {
+        if(collidedObj->name.find("obstacle") != std::string::npos)
+        {
+            riding = true;
+        }
+    }
+    else if (collidedObj->name.find("enemy") != std::string::npos && !hit)
+    {
+        Hit(dt);
+    }
 }
 
 void Player::Render(Renderer& renderer, const Camera& camera)
 {
-    renderer.DrawTexturedQuad(*mesh, transform, camera, AssetManager::GetShader(shaderName), texture, {1.0f, 1.0f, 1.0f, 0.0f});
+    renderer.DrawTexturedQuad(*mesh, transform, camera, AssetManager::GetShader(shaderName), texture, color);
     texture.Unbind();
 }
 
@@ -84,6 +109,6 @@ void Player::Hit(float dt)
     rigidBody.velocity.x *= -1;
     jumping = true;
     hit = true;
-    hp -= dt;
+    hp--;
     Jump();
 }
